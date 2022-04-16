f033468c899773ff9e7704f51a2323c8ca9e1400
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:03:18 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/pragmatickm/procedure/servlet/impl/ProcedureImpl.java
+++ src/main/java/com/pragmatickm/procedure/servlet/impl/ProcedureImpl.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with pragmatickm-procedure-servlet.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.pragmatickm.procedure.servlet.impl;
 
 import com.aoapps.html.any.AnyPalpableContent;
